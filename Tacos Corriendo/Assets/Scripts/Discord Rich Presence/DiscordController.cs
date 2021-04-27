3dee94620c7b5b4d0e417096b5af14674bd0bc90
DJn0cide <wassimbenamarpro@gmail.com>
2021-04-28 00:01:15 +0400

    Discord rich presence (and stuff)

---
+++ Tacos Corriendo/Assets/Scripts/Discord Rich Presence/DiscordController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Discord;
+
+public class DiscordController : MonoBehaviour
+{
+
+    public Discord.Discord discord;
+    [SerializeField] string Details;
+    [SerializeField] string State;
+    [SerializeField] int StartTimestamp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        discord = new Discord.Discord(836680203131551764, (System.UInt64)Discord.CreateFlags.Default);
+        var activityManager = discord.GetActivityManager();
+        var activity = new Discord.Activity
+        {
+            Details = Details,
+            State = State,
+            Timestamps =
+            {
+                Start = 5,
+            },
+        };
+        activityManager.UpdateActivity(activity, (result) =>
+        {
+            if (result == Discord.Result.Ok)
+            {
+                Debug.Log("Successfully updated Discord Rich Presence");
+            }
+            else
+            {
+                Debug.LogError("Failed to update Discord Rich Presence");
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        discord.RunCallbacks();
+    }
+
+    void OnApplicationQuit()
+    {
+        
+        discord.Dispose();
+    }
+}
