dabee3b5f01654f7779080b481ee61ebd83fb500
pigeonsownme <hijaziamm@gmail.com>
2021-05-15 13:51:54 +0400

    main.exe fixed

--- Tacos Corriendo/Assets/Scripts/WorldGen/LevelGenerator/py/main.py
+++ Tacos Corriendo/Assets/Scripts/WorldGen/LevelGenerator/py/main.py
@@ -1,12 +1,23 @@
 from levelgenerator import Level
 import sys
+import os
 
 args = [int(arg) for arg in sys.argv[1:]]
 
 level = Level(width=args[0], height=args[1])
-level.GenerateForks(min_forks=args[2], max_forks=args[3], min_distance=args[4])
+level.GenerateForks(
+    min_forks=args[2],
+    max_forks=args[3],
+    min_distance=args[4],
+    max_per_row=1,
+    max_per_column=1,
+)
 level.GenerateRoads()
 level.FixDeadEnds()
+level.ConvertRoadTypes()
+level.Print()
 
-with open("level.json", "w") as f:
-    level.ToJSON(f)
+os.chdir(os.getcwd() + "\\Assets\\Scripts\\WorldGen\\LevelGenerator\\")
+
+with open("level.tcl", "w") as f:
+    f.write(level.ToTCL())
