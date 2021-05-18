7fb9cec0892c573b7af1c25d6905e11a36620c67
pigeonsownme <hijaziamm@gmail.com>
2021-05-18 17:55:29 +0400

    piss

--- Tacos Corriendo/Assets/Scripts/People/People.cs
+++ Tacos Corriendo/Assets/Scripts/People/People.cs
@@ -1,22 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class People : MonoBehaviour
 {
     [Range(0, 6)]
     [SerializeField] int MaxSize;
-    [HideInInspector]
     public int GroupSize;
+    bool road = false;
+    public bool hasSpawned;
+    GameObject TACOs;
+    GameObject spawned;
     // Start is called before the first frame update
     void Start()
     {
-        GroupSize = Random.RandomRange(0, MaxSize);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void pplnshit()
+    {
+        if (!hasSpawned)
+        {
+            GroupSize = Random.RandomRange(0, MaxSize);
+            TACOs = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Graphics/Prefab/UI/Taco.prefab", typeof(GameObject));
+            spawned = Instantiate(TACOs, new Vector3(0.5f, 2, -0.5f), Quaternion.Euler(0,0,0));
+            spawned.transform.parent = transform;
+            Destroy(GetComponent<Rigidbody>());
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Road")
+        {
+            road = true;
+            pplnshit();
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (!hasSpawned)
+        {
+            if (other.tag == "Road")
+            {
+                road = true;
+                pplnshit();
+            }
+        }
     }
 }
